05148fc61a759b8067c04a556b25e8e91420bba0
AlexCSS <kalyuzhnyi@gmail.com>
2021-11-18 17:58:25 +0200

    init

---
+++ calc/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <h2 class="section-title text-center">Estimate your price</h2>
+
+    <a href="" class="button btn btn-primary">
+      Get Started
+    </a>
+  </div>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return {
+      isLoading : true,
+      api: "https://neonpanel.com/php/api.php",
+      data : []
+    }
+  },
+  methods:{
+    getData() {
+      this.isLoading = true;
+      axios
+          .get(this.api)
+          .then(response => {
+            this.data = response.data;
+            console.log(this.data);
+            this.isLoading = false
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    }
+  },
+  mounted() {
+    this.getData()
+  }
+}
+</script>
+
+<style lang="scss">
+.button{
+  color: #fff;
+  background: #5956E9;
+  border:1px solid #5956E9;
+  border-radius: 5px 5px;
+  padding: 15px 30px;
+  display: inline-block;
+  font-size: 15px;
+  line-height: 24px;
+  font-weight: 600;
+  margin: 30px 0 5px 0;
+  transition: all 0.3s ease-in-out;
+  -webkit-transition: all 0.3s ease-in-out;
+}
+</style>
---
+++ php/api.php
@@ -0,0 +1,32 @@
+<?php
+
+header("Content-Type:application/json");
+
+try {
+	$database_name     = 'joomallc_neonpanel';
+	$database_user     = 'joomallc_neonpanel';
+	$database_password = '^gYd4RfJi#gT';
+	$database_host     = 'localhost';
+
+	$pdo = new PDO('mysql:host=' . $database_host . '; dbname=' . $database_name, $database_user, $database_password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+	$sql = 'select * from pricing';
+
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+
+	$data = [];
+
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$data[] = $row;
+	}
+
+	$response         = [];
+	$response['data'] =  $data;
+
+	echo json_encode($response, JSON_PRETTY_PRINT);
+
+} catch (PDOException $e) {
+	echo 'Database error. ' . $e->getMessage();
+}
